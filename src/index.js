fe8e9f2d70c8691e851231eb72a671bf2b713d8d
Luong Duc Tuan Dat <luongductuandat.psa@gmail.com>
2019-03-06 16:03:05 +0700

    Edit to deploy

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('rootpath')();
+const express = require('express')
+const app = express()
+const port = 8081
+app.use(express.static('dist'));
+
+app.get('/*', (req, res) => {
+  // console.log('in f5');
+  res.sendFile(__dirname  + 'dist/index.html', err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
