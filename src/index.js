1f9b108154ba326de2af0df6d3950df1c0248ac5
Luong Duc Tuan Dat <luongductuandat.psa@gmail.com>
2019-03-06 16:17:55 +0700

    Fix path sendFile in index.js 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.static('dist'));
 
 app.get('/*', (req, res) => {
   // console.log('in f5');
-  res.sendFile(__dirname  + '../dist/index.html', err => {
+  res.sendFile(__dirname  + '/../dist/index.html', err => {
     if (err) {
       res.status(500).send(err);
     }
